9018e6e6b5c67bf83d6e125375accb2f4f6cf7c3
dnunez10 <diana_nunez10@yahoo.com>
2021-02-25 22:52:16 -0800

    commit tree 980d2cdad6a90c361cb2574d6882f12b3ee0748d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+//Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) {
+        return blocks - 42
+    }else {
+        return (42 - blocks);
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+    return 264 * distanceFromHqInBlocks(blocks);
+}
+
+function distanceTravelledInFeet(a, b) {
+    if (a > b) {
+        return 264 * (a - b)
+    }else {
+        return 264 * (b - a);
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    if (destination - start === 1) {
+        return 264 * (destination - start) * 0
+    }else if ((start - destination) * 264 > 400 && (start - destination) * 264 < 2000) {
+        return ((264 * (start - destination)) - 400) * .02
+    }else if ((destination - start) * 264 > 2000) {
+        return 25  
+    }else if ((start - destination) * 264 === 2640) {
+        return 'cannot travel that far'
+    }
+}
---
+++ testing.js
@@ -0,0 +1,9 @@
+function breakfast(potato) {
+    //.log('I love breakfast')
+    potato();
+}
+breakfast(lunchTime);
+
+function lunchTime() {
+    console.log('lunch is coming up')
+}
